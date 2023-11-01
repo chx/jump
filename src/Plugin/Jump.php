695dbcd8d794aad3a2f4c042021764a518c1cc77
Karoly Negyesi <chx1975@gmail.com>
2023-11-01 09:31:17 +0100

    initial commit

---
+++ src/Plugin/Jump.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace chx\Composer\Plugin;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Jump implements PluginInterface {
+
+  public function activate(Composer $composer, IOInterface $io) {
+  }
+
+  public function deactivate(Composer $composer, IOInterface $io) {
+  }
+
+  public function uninstall(Composer $composer, IOInterface $io) {
+  }
+
+}
