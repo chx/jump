927908508222a4ba114cde4fabbc300b044aafe4
Karoly Negyesi <chx1975@gmail.com>
2023-11-01 13:58:52 +0100

    capable was missing

--- src/Plugin/Jump.php
+++ src/Plugin/Jump.php
@@ -6,9 +6,10 @@ use chx\Composer\JumpCommandProvider;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\Capability\CommandProvider;
+use Composer\Plugin\Capable;
 use Composer\Plugin\PluginInterface;
 
-class Jump implements PluginInterface
+class Jump implements PluginInterface, Capable
 {
 
     public function activate(Composer $composer, IOInterface $io)
